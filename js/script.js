b2bea662ea872f0fc93f9d89bb4d2119f39b4d5c
RedSanchezz <redsanchezz@yandex.ru>
2021-07-29 14:09:45 +0300

    first

---
+++ js/script.js
@@ -0,0 +1,80 @@
+const header = document.querySelector('#header_block');
+const headerContent = document.querySelector('.header-content');
+const headerBg = document.querySelector('.header-bg');
+const imgTablet = document.querySelector('#tablet_img');
+const lineArrow  = document.querySelector('.header-arrow');
+const lineImg = document.querySelector('#img_arrow');
+const headerArrowText = document.querySelector('.header-arrow-text');
+const container = document.querySelector('.container');
+const imgArrow2 = document.querySelector('#img_arrow2');
+
+const modalWindow = document.querySelector('.modal-video');
+const modalWindowBtn = document.querySelector('#watch-video');
+const modalCloseBtn = document.querySelector('.modal-video__back-btn');
+
+
+modalWindowBtn.addEventListener('click', (e) => {
+    modalWindow.classList.add('active');
+})
+
+modalCloseBtn.addEventListener('click', (e) => {
+    modalWindow.classList.remove('active');
+})
+
+window.onload = ()=>{
+
+    setHeaderBgSize();
+    getArrowCoord();
+
+    addEventListener('resize',  ()=>{
+        setHeaderBgSize();
+        getArrowCoord();
+    });
+    
+    function setHeaderBgSize(){
+        headerBg.style.height = getComputedStyle(headerContent).height;
+        header.style.height = getComputedStyle(headerContent).height;
+    }
+    
+    function getArrowCoord (){
+
+        let clientRectImgTablet = imgTablet.getBoundingClientRect();
+        //если мобильные стили - стрелка должна быть под упаковкой
+        if(window.matchMedia('(max-width: 768px)').matches){
+            lineImg.src = 'images/mobile-arrow.png';
+            lineArrow.style.width='100%'
+            lineArrow.style.top = clientRectImgTablet.top + clientRectImgTablet.height + 9 +  pageYOffset + 'px';
+            
+            getTextCoord();
+            return;
+        }
+
+        let width =  clientRectImgTablet.left + clientRectImgTablet.width - 80  +'px';
+        let height = parseInt(getComputedStyle(lineArrow).height);
+        lineArrow.style.width = width;
+        lineArrow.style.top = clientRectImgTablet.top + clientRectImgTablet.height  - height + pageYOffset -60 + 'px';
+        //координаты текста
+        getTextCoord();
+        //Задаем координаты второй линии
+        imgArrow2.style.width = window.innerWidth - clientRectImgTablet.left - 100 +'px';
+        console.log(window.innerWidth - clientRectImgTablet.left+'px');
+
+
+    }
+    function getTextCoord(){
+
+        let lineArrowCoord = parseInt(lineArrow.style.top) + lineArrow.getBoundingClientRect().height/3;
+        console.log(lineArrowCoord);
+        // headerArrowText.style.top = clientRectImgTablet.bottom + pageYOffset - 105 + 'px';
+        headerArrowText.style.top = lineArrowCoord + 'px';
+        textLeftPosition =  container.getBoundingClientRect().left + 15 + 'px';
+        // headerArrowText.style.left = textLeftPosition; 
+    }
+}
+
+
+let clue = document.querySelector('.ask-question__checkbox-question');
+let icon = document.querySelector('.ask-question__question-icon');
+icon.addEventListener('click', (e) => {
+    clue.classList.toggle('active');
+})
---
+++ js/slider.js
@@ -0,0 +1,68 @@
+
+const sections = document.querySelectorAll('section');
+const headerBlock = document.querySelector('header');
+const slides = [headerBlock, ...sections];
+const texts = [
+            'Бережная обработка нужна каждому питомцу',
+            'Защита от гельминтов: главное',
+            'Меняйте жизнь питомца к лучшему',
+            'Ассортимент',
+            'Где купить',
+            'Задать вопрос ветеринарному врачу'
+]
+const navMenu = document.querySelector('.navigation-menu');
+const navMenuText = document.querySelector('.navigation-menu__active-window-text');
+const whereBuy = document.querySelector('.navigation-menu__where-buy');
+
+
+
+
+let activeBlockNumber = Math.ceil(pageYOffset/window.innerHeight);
+setCurrentSlide(activeBlockNumber);
+
+if(activeBlockNumber===0){
+    navMenu.style.display='none';
+}else{
+    navMenu.style.display='';
+}
+
+whereBuy.addEventListener('click', () => {
+    setCurrentSlide(5);
+})
+
+function wheel(e){
+    if(e.deltaY>0){
+        if(activeBlockNumber <slides.length -1) setCurrentSlide(activeBlockNumber+1);
+    }
+    else {
+        if(activeBlockNumber>0) setCurrentSlide(activeBlockNumber-1);
+
+    }
+}
+window.addEventListener('wheel', wheel);
+
+function setCurrentSlide(number){
+    navMenu.classList.remove('active');
+    if(window.matchMedia('(max-width: 768px)').matches) return;
+    if(number===0){
+        navMenu.style.display='none';
+    }else{
+        navMenu.style.display='';
+    }
+    window.removeEventListener('wheel', wheel);
+    setTimeout(() => {
+        window.addEventListener('wheel', wheel);
+    }, 300);
+    
+    let top = slides[number].getBoundingClientRect().top + pageYOffset;
+    window.scroll({
+        top: top,
+        behavior: 'smooth'
+    });
+    navMenuText.innerHTML=texts[number-1];
+    activeBlockNumber = number;
+
+    // console.log('Y offset '+pageYOffset);
+    // scrollTo(0, clientRect.top + pageYOffset);
+}
+
